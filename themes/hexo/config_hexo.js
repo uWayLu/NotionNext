39f151a3451e3e656c9671ccffd334364bf0445e
uWayLu <u.master.o.twn@gmail.com>
2022-03-18 16:01:32 +0800

    theme: update hexo banner desc

--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -1,6 +1,6 @@
 const CONFIG_HEXO = {
   HOME_BANNER_ENABLE: true,
-  HOME_BANNER_GREETINGS: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
+  HOME_BANNER_GREETINGS: ['你好，我是塵世迷途小碼農', '歡迎來到我的部落格🎉', '本站好記網址 uwaylu.cf', 'uwaylu for your confer'], // 首页大图标语文字
   HOME_BANNER_IMAGE: './bg_image.jpg', // see /public/bg_image.jpg
 
   // 菜单配置
